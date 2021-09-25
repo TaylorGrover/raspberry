c49739364a21002e2b10806970fcb77da65523c0
TaylorGrover <taylorz987@gmail.com>
2021-09-25 15:17:53 -0600

    Changed directory structure slightly.

--- fix_images.py
+++ fix_images.py
@@ -3,9 +3,6 @@ import os
 import PIL.Image as Image
 import sys
 
-digit_map = {"0" : "zero", "1" : "one", "2" : "two", "3" : "three", "4" : "four", "5" : "five", 
-            "6" : "six", "7" : "seven", "8" : "eight", "9" : "nine"}
-
 def shrink_image(img):
     return img.resize(28, 28)
 
@@ -18,37 +15,38 @@ def main():
         print("Usage: fix_images DIGIT")
         sys.exit(1)
 
-    ## The command-line argument (a digit between 0 - 9) is the label used for the images
+    ## The first command-line argument (a digit between 0 - 9) is the label used for the images
     label = sys.argv[1]
-
-    ## If the label isn't a valid directory, exit.
-    if not os.path.isdir(label):
-        print("Invalid argument.")
-        sys.exit(1)
-
-    ## Check the selected "label" directory for existing images files. Obtain last image name
-    imgfiles = os.listdir(label)
-    if len(imgfiles) > 0:
-        numeric_suffix = int(imgfiles[-1].split("_")[1].split(".")[0])
-    else:
-        numeric_suffix = 0
 
     ## The digits directory is the landing place for raw images. 
     # Images will resized and labeled and greyscaled from this directory, then the smaller
     # copies will be moved to the appropriate directories. 
     landing_dir = "digits/"
-    imgfiles = os.listdir(landing_dir + label)
+    temp_files = os.listdir(landing_dir)
+    print(temp_files)
     
+    ## If the label isn't a valid directory, exit.
+    """if not os.path.isdir(label):
+        print("Invalid argument.")
+        sys.exit(1)"""
+
+    ## Check the selected "label" directory for existing images files. Obtain last image name
+    imgfiles = os.listdir("data/greyscale/" + label)
+    if len(imgfiles) > 0:
+        numeric_suffix = int(imgfiles[-1].split("_")[1].split(".")[0])
+    else:
+        numeric_suffix = 0
+
     ## The numeric suffix + 1 will be the starting position of the new images
-    for imgfile in imgfiles:
+    for tmp in temp_files:
         numeric_suffix += 1
-        with Image.open(landing_dir + label + "/" + imgfile) as raw_img:
+        with Image.open(landing_dir + tmp) as raw_img:
             grey_array = greyscale(raw_img)
             resized_img = Image.fromarray(grey_array).resize((28, 28), Image.BICUBIC)
-            name = label + "/" + digit_map[label] + "_" + str(numeric_suffix) + ".png"
+            name = "data/greyscale/" + label + "/" + label + "_" + str(numeric_suffix) + ".png"
             print(name)
             resized_img.convert("RGB").save(name)
-        os.remove(landing_dir + label + "/" + imgfile)
+        os.remove(landing_dir + tmp)
 
 if __name__ == "__main__":
     main()
---
+++ to_binary.py
@@ -0,0 +1,14 @@
+""" Program to fix the RGB greyscaled png files and turn them into bytearrays,
+then deposit them into the binary files along with their corresponding labels.
+"""
+
+import numpy as np
+import os
+import PIL.Image as Image
+import sys
+
+toplevel_dir = "data/"
+
+""" 
+
+"""
