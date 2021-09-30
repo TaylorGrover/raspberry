Attempting to fix goddamn rotation.

.ipynb_checkpoints/image_editor-checkpoint.py
@@ -28,7 +28,7 @@ class Application(tk.Frame):
                     self.images.append(np.array(img))
 
     def set_prev(self, event = None):
-        self.index -= 2
+        self.index -= 1
         self._update_image()
 
     def set_next(self, event = None):
@@ -46,7 +46,7 @@ class Application(tk.Frame):
 
     def _update_image(self):
         self.index %= len(self.images)
-        print(self.paths[self.index])
+        print(self.index)
         img = ImageTk.PhotoImage(Image.fromarray(self.images[self.index]).resize((256, 256), Image.BICUBIC))
         self.panel.configure(image = img)
         self.panel.image = img

fix_images.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import PIL.Image as Image
-from scipy.dimage import rotate
+from scipy.ndimage import rotate
 import sys
 import time
 
@@ -51,9 +51,12 @@ def main():
         with Image.open(landing_dir + label + "/" + tmp) as raw_img:
             grey_array = greyscale(raw_img)
             resized_img = Image.fromarray(grey_array).resize((28, 28), Image.BICUBIC)
+            # Prevent unexpected rotation
+            resized_img = rotate(resized_img, -90)
             name = "data/greyscale/" + label + "/" + label + "_" + str(numeric_suffix) + ".png"
             print(name)
             resized_img.convert("RGB").save(name)
+
         os.remove(landing_dir + label + "/" + tmp)
         # Prevent program crashing on mobile device
         time.sleep(.01)

main.py
@@ -11,8 +11,8 @@ batch_size = 20
 clip_threshold = .01
 eta = .3
 decay = 0
-architecture = [784, 80, 10]
-activations = ["nrelu", "softargmax"]
+architecture = [784, 20, 10]
+activations = ["sigmoid", "softargmax"]
 
 np.random.seed(1)
 nn = NeuralNetwork(architecture, activations)
@@ -23,10 +23,12 @@ nn.use_clipping = False
 nn.use_dropout = False
 nn.use_L2 = False
 nn.show_gradient = False
-nn.save_wb = True
+nn.save_wb = False
 nn.use_diff_eq = True
 
-training_images, training_labels, validation_images, validation_labels, testing_images, testing_labels = get_noisy_mnist()
+#training_images, training_labels, validation_images, validation_labels, testing_images, testing_labels = get_noisy_mnist()
+training_images, training_labels, validation_images, validation_labels = get_training_and_validation(inverted = False)
+testing_images, testing_labels = get_testing_images(inverted = False)
 if len(sys.argv) == 3:
    wb_filename = sys.argv[1]
    nn.set_wb(wb_filename)